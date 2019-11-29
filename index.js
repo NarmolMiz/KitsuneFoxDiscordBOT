8469637f973686da4f00ff7dcf2b14f4c5eb671d
NarmolMiz <58323347+NarmolMiz@users.noreply.github.com>
2019-11-29 18:11:43 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-  if (message.content === '/ping') {
+  if (message.content === `${prefix}ping`) {
     message.channel.send('Pong.');
+  } else if (message.content === `${prefix}beep`) {
+    message.channel.send('Boop.');
+  } else if (message.content === `${prefix}server`) {
+    message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+  } else if (message.content === `${prefix}user-info`) {
+    message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
   }
 });
 
