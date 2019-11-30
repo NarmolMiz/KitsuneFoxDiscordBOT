f14edb676dde5be5e459e2021302a7f15a5995cc
NarmolMiz <58323347+NarmolMiz@users.noreply.github.com>
2019-11-30 09:30:45 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -13,14 +13,19 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.content === `${prefix}ping`) {
     message.channel.send('Pong.');
+    console.log('Used command : ping')
   } else if (message.content === `${prefix}beep`) {
     message.channel.send('Boop.');
+    console.log('Used command : beep')
   } else if (message.content === `${prefix}server`) {
     message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-  } else if (message.content === `${prefix}user-info`) {
+    console.log('Used command : server')
+  } else if (message.content === `${prefix}userinfo`) {
     message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+    console.log('Used command : userinfo')
   } else if (message.content === `${prefix}amessage`) {
     message.channels.get('648562428640231434').send(message.attachments.url)
+    console.log('Used command : amessage')
   }
 });
 
