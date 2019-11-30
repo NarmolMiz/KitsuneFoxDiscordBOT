a1968e5eea09d91f797849ac34e8cf431ec19eae
NarmolMiz <58323347+NarmolMiz@users.noreply.github.com>
2019-11-30 09:44:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,30 +4,55 @@ const prefix = '/'
 
 client.on('ready', () => {
   console.log('Bot as been planted !!')
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
 })
 
-
 //-------------------------------------------------------
+
+client.on("guildCreate", guild => {
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+//-----------------------------------------------------
+
+client.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+//----------------------------------------------------
 
 
 client.on('message', message => {
   if (message.content === `${prefix}ping`) {
     message.channel.send('Pong.');
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
     console.log('Used command : ping')
+
   } else if (message.content === `${prefix}beep`) {
     message.channel.send('Boop.');
     console.log('Used command : beep')
+
   } else if (message.content === `${prefix}server`) {
     message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
     console.log('Used command : server')
+
   } else if (message.content === `${prefix}userinfo`) {
     message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
     console.log('Used command : userinfo')
+
   } else if (message.content === `${prefix}amessage`) {
     message.channels.get('648562428640231434').send(message.attachments.url)
     console.log('Used command : amessage')
   }
 });
+
+//--------------------------------------------------------
+
+
 
 //--------------------------------------------------------
 
