435c6bc180c45de32ec21b50929f506a0ba91605
NarmolMiz <58323347+NarmolMiz@users.noreply.github.com>
2019-12-02 17:51:49 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -23,9 +23,23 @@ client.on("guildDelete", guild => {
 
 //----------------------------------------------------
 
-client.on("guildMemberAdd", member => {
-  console.log(`Member ${member.name} has joined ${member.name}`)
-  channel.get('648562428640231434').send(`**${member.name} a rejoint le serveur !! :joy:**`)
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.find(channel => channel.name === "general").send("Welcome our new users!\n" + userlist);
+    newUsers[guild.id].clear();
+  }
+});
+
+//---------------------------------------------------
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
 });
 
 //---------------------------------------------------
