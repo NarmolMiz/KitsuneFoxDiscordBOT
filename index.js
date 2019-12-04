dbefb6c068089fb41cb173c2b79f44efce924caf
NarmolMiz <58323347+NarmolMiz@users.noreply.github.com>
2019-12-04 11:44:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.on('ready', () => {
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
         client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-    }, 10000); // Runs this every 10 seconds.
+    }, 1000); // Runs this every 1 seconds.
 });
 
 //-------------------------------------------------------
